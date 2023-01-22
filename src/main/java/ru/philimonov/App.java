2e046259dad95af2869857c3bd374c9fb16c42ef
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-22 17:53:11 +0300

    save Person to DB with Hibernate

--- src/main/java/ru/philimonov/App.java
+++ src/main/java/ru/philimonov/App.java
@@ -13,8 +13,15 @@ public class App {
         Session session = factory.getCurrentSession();
         try {
             session.beginTransaction();
-            Person person = session.get(Person.class, 1);
-            System.out.println(person);
+            Person person1 = new Person("Test Person 1", 25);
+            Person person2 = new Person("Test Person 2", 26);
+            Person person3 = new Person("Test Person 3", 27);
+            Person person4 = new Person("Test Person 4", 28);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+            session.save(person4);
             session.getTransaction().commit();
         } catch (HibernateException e) {
             throw new RuntimeException(e);
